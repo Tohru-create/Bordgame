d8531e5e497180da5b5d2c4037281647e1c56c0d
Haruto Miyai <195989610+harutomiyai@users.noreply.github.com>
2025-02-27 13:21:04 +0900

    ストーリー表示機能のHTMLファイルを追加し、スクロール速度を調整

--- story/script.js
+++ story/script.js
@@ -7,7 +7,7 @@ function scrollStep() {
     if (container.scrollTop + container.clientHeight >= container.scrollHeight) {
         container.scrollTop = 0; // 最上部に戻る
     } else {
-        container.scrollTop += 0.7; // スクロールの速度（調整可）
+        container.scrollTop += 0.5; // スクロールの速度（調整可）
     }
 
     setTimeout(() => requestAnimationFrame(scrollStep), 10); // 速度調整
